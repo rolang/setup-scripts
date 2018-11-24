4a7ab79c5c0ad463bfcb966cb73592a17f614618
Roman Langolf <pm@rolang.codes>
2018-11-24 20:19:02 +0100

    fix missing plug.vim installation

--- vim-conf.sh
+++ vim-conf.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -e
+
+sudo apt install exuberant-ctags ncurses-term -y
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 declare -a langs=(
 "javascript"
