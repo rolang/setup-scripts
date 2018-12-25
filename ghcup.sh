763d5b36794d7c5356838a23a7e27b21aa741128
Roman Langolf <pm@rolang.codes>
2018-12-25 14:14:57 +0100

    update ghcup script

--- ghcup.sh
+++ ghcup.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-GHCUP_HOME="\$HOME/.ghcup/bin"
+set -e
 
-(\
-    mkdir -p ~/.ghcup/bin && \
-    curl https://raw.githubusercontent.com/haskell/ghcup/master/ghcup > ~/.ghcup/bin/ghcup && \
-    chmod +x ~/.ghcup/bin/ghcup \
-) && echo "Success"
+GHCUP_HOME=$HOME/.ghcup/bin
+
+mkdir -p ~/.ghcup/bin
+curl -s https://raw.githubusercontent.com/haskell/ghcup/master/ghcup > $GHCUP_HOME/ghcup
+chmod +x $GHCUP_HOME/ghcup
 
 if [ $(cat ~/.bashrc | grep -c "$GHCUP_HOME:\$PATH") -lt 1 ] ; then
 cat >> ~/.bashrc <<EOF
 export PATH="\$HOME/.cabal/bin:$GHCUP_HOME:\$PATH"
 EOF
 fi
+
+echo "Installed in $GHCUP_HOME"
+$GHCUP_HOME/ghcup -h
