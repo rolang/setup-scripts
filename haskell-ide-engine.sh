5df83a8d9937504f2c1e8134b5ffe65a9963fa75
Roman Langolf <pm@rolang.codes>
2019-04-22 18:38:55 +0200

    add hie installation script

---
+++ haskell-ide-engine.sh
@@ -0,0 +1,28 @@
+!#/usr/bin/env bash
+
+sudo apt install libicu-dev libtinfo-dev libgmp-dev -y
+
+if [ ! -z $(hie --numeric-version) ]; then
+  echo "hie $(hie --numeric-version) already installed"
+  echo "latest release version on github: $(curl -s https://api.github.com/repos/haskell/haskell-ide-engine/releases/latest | jq .tag_name)"
+
+  if [ "$1" != "-f" ]; then
+    echo "run with -f flag to force rebuild"
+    exit 0
+  fi
+fi
+
+mkdir -p ~/projects
+cd ~/projects
+
+HIE_PROJECT_NAME="haskell-ide-engine"
+
+if [ -d $HIE_PROJECT_NAME ]; then
+  echo "hie is already checked out, trying to update..."
+  cd $HIE_PROJECT_NAME && git pull && ./install.hs build-all
+else
+  echo "checking out hie..."
+  git clone https://github.com/haskell/$HIE_PROJECT_NAME --recursive
+  cd $HIE_PROJECT_NAME && ./install.hs build-all
+fi
+
