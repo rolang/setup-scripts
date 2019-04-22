9af1f33db766bbb6000ce1e111735c597f7e4c6d
Roman Langolf <pm@rolang.codes>
2019-04-22 17:43:30 +0200

    add cargo to path on rust install

--- rust.sh
+++ rust.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 curl https://sh.rustup.rs -sSf | sh
+
+# add cargo to path
+if [ $(cat ~/.bashrc | grep -c ".cargo/bin:\$PATH") -lt 1 ] ; then
+cat >> ~/.bashrc <<EOF
+export PATH="$HOME/.cargo/bin:\$PATH"
+EOF
+fi
+
