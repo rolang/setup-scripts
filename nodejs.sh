602b51036f5c5e5176961e6d67aaf9e51a94fed9
Roman Langolf <pm@rolang.codes>
2019-04-28 11:41:52 +0200

    fix nvm setup

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NVM_VERSION=$(curl -s 'https://api.github.com/repos/creationix/nvm/releases/latest' | jq -r '.tag_name')
+NVM_VERSION=$(curl -sL 'https://api.github.com/repos/creationix/nvm/releases/latest' | jq -r '.tag_name')
 curl -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
