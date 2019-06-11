26f0e4f9edcecf494de4bae3ac6bd679dccadaea
Roman Langolf <pm@rolang.codes>
2019-06-11 10:44:32 +0200

    fix docker install

--- docker.sh
+++ docker.sh
@@ -5,7 +5,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+   stable edge test"
 
 sudo apt install docker-ce -y
 sudo groupadd docker
