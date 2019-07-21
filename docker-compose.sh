c5f77b993c2d43ade729dffc14fc5d515a767839
Roman Langolf <pm@rolang.codes>
2019-07-21 13:04:43 +0200

    add docker-compose shortcut to path

--- docker-compose.sh
+++ docker-compose.sh
@@ -6,4 +6,5 @@ echo "installing docker-compose version ${DOCKER_COMPOSE_VERSION}"
 
 sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose ~/.local/bin/dc
 docker-compose --version
