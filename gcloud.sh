53eb50d4fc6cf6b569a3af2c2ba303846aa579fc
Roman Langolf <pm@rolang.codes>
2020-11-08 10:58:39 +0000

    install sql proxy with gcloud

--- gcloud.sh
+++ gcloud.sh
@@ -6,8 +6,11 @@ if [ ! -f "/etc/apt/sources.list.d/google-cloud-sdk.list" ]; then
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 fi
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt update && sudo apt install google-cloud-sdk kubectl -y
+
+curl -s https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -o ~/.local/bin/cloud_sql_proxy && chmod +x ~/.local/bin/cloud_sql_proxy
+echo "Installed $(cloud_sql_proxy --version)"
 
 add_to_bashrc "source <(kubectl completion bash)"
 
