35cd824550d4a6b52039612f8a3665515de68889
Roman Langolf <rolang@pm.me>
2021-06-05 11:54:47 +0100

    update gcloud install

--- gcloud.sh
+++ gcloud.sh
@@ -2,9 +2,7 @@
 
 source ./functions.sh
 
-if [ ! -f "/etc/apt/sources.list.d/google-cloud-sdk.list" ]; then
-  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-fi
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt update && sudo apt install google-cloud-sdk kubectl -y
