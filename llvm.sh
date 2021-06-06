469c8aa5fbd38d1ace8e88e6a6cb79eb331c5447
Roman Langolf <rolang@pm.me>
2021-06-06 11:49:47 +0100

    update llvm setup

--- llvm.sh
+++ llvm.sh
@@ -4,5 +4,8 @@ VERSION=12
 CODE=$(lsb_release -cs)
 curl -s https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb http://apt.llvm.org/${CODE}/ llvm-toolchain-${CODE}-${VERSION} main"
-sudo apt-get update && sudo apt-get install clang-${VERSION} lldb-${VERSION} lld-${VERSION} -y
+
+sudo apt-get update && sudo apt-get install clang-${VERSION} lldb-${VERSION} lld-${VERSION} -y && \
+  sudo ln -sf /usr/bin/clang-${VERSION} /usr/bin/clang && \
+  sudo ln -sf /usr/bin/clang++-${VERSION} /usr/bin/clang++
 
