ba3fc021528598a06b333b0263384d8670cb4984
Roman Langolf <rolang@pm.me>
2021-10-07 20:59:59 +0100

    add postgresql-client install script, update scala

--- scala.sh
+++ scala.sh
@@ -4,7 +4,11 @@ set -e
 
 source ./functions.sh
 
-SCALA_VERSION=$(curl -s 'https://api.github.com/repos/scala/scala/releases/latest' | jq .tag_name | grep -oE '[0-9\.]+')
+SCALA_VERSION=2.13.6 
+
+# latest release
+# SCALA_VERSION=$(curl -s 'https://api.github.com/repos/scala/scala/releases/latest' | jq .tag_name | grep -oE '[0-9\.]+')
+
 SCALA_HOME=~/.scala
 
 wget -O- https://downloads.lightbend.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz | tar xz -C ~/
